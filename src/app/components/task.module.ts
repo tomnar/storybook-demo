4af1b5a5462b3b2d44a7bd1c590f6c20f7bb554b
Tom Quast <tom@konsulentdb.dk>
2022-12-15 08:21:59 +0100

    Setup examples

---
+++ src/app/components/task.module.ts
@@ -0,0 +1,14 @@
+
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TaskComponent } from './task.component';
+import { TaskListComponent } from './task-list.component';
+
+@NgModule({
+  imports: [CommonModule],
+  exports: [TaskComponent, TaskListComponent],
+  declarations: [TaskComponent, TaskListComponent],
+  providers: [],
+})
+export class TaskModule {}
